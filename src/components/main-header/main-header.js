5e5cc3e01fb8e72c1692bc91a0df18a86586e8dd
Guido Asbun <guido@asbun.io>
2024-04-08 17:28:06 -0700

    Authentication first test

--- src/components/main-header/main-header.js
+++ src/components/main-header/main-header.js
@@ -1,27 +1,36 @@
 import Link from "next/link";
+import { UserButton } from "@clerk/nextjs";
 
 export default function MainHeader() {
   return (
     <header className="p-10">
-      <p className="text-4xl font-bold text-left">Main Navigation - Header</p>
-      <nav>
-        <ul>
-          <li>
-            <Link href="/">* Home</Link>
+      <nav
+        className="flex items-center justify-between p-6 lg:px-8 h-20 border border-t-0 border-l-0 border-b-gray-600"
+        aria-label="Global"
+      >
+        <div className="flex lg:flex-1">
+          <a href="/" className="-m-1.5 p-1.5">
+            Chef-AI
+          </a>
+        </div>
+        <ul className="flex lg:flex-1">
+          <li className="pr-10">
+            <Link href="/">Home</Link>
           </li>
-          <li>
-            <Link href="/enterIngredients">* Enter Ingredients</Link>
+          <li className="pr-10">
+            <Link href="/enterIngredients">Enter Ingredients</Link>
           </li>
-          <li>
-            <Link href="/waitPage">* Wait Page</Link>
+          <li className="pr-10">
+            <Link href="/waitPage">Wait Page</Link>
           </li>
-          <li>
-            <Link href="/recipeSelection">* Recipe Selection</Link>
+          <li className="pr-10">
+            <Link href="/recipeSelection">Recipe Selection</Link>
           </li>
-          <li>
-            <Link href="/recipeSelection/delicious-tacos">
-              * Selected Recipe (delicious tacos = slug)
-            </Link>
+          <li className="pr-10">
+            <Link href="/recipeSelection/delicious-tacos">Selected Recipe</Link>
+          </li>
+          <li className="pr-10">
+            <UserButton afterSignOutUrl="/" />
           </li>
         </ul>
       </nav>
