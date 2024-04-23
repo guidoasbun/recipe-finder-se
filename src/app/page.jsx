4221a98a18329e59538c375b6e74c1352b13496a
adriancancode <vazqueza2004@gmail.com>
2024-04-23 16:15:12 -0700

    Add welcome text and rescale buttons

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,11 +4,17 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-gradient-to-tl from-sky-500 to-white-500">
-      <p className="text-6xl font-bold text-center">Chef-AI</p>
-      <div></div>
-      <button className="bg-slate-300 hover:bg-sky-200 border-2 py-5 px-20 text-lg rounded-2xl mx-10 pb-10">Log In</button>
-     
-      <button className="bg-transparent border-2 py-5 px-20 text-lg rounded-2xl hover:bg-slate-300">Sign Up</button>
+      <h1 className="text-8xl font-bold ">Chef-AI</h1>
+      <Image src="/spaghetti.png" width={89} height={55} alt="Delicious spaghetti in a glass tray"/>
+      <p className='text-3xl'>Welcome to Chef-AI!  With the power of AI, this app allows for fast and reliable recipe searches with any ingredients in 
+        your pantry!
+      </p>
+      <p className='text-5xl'>Let's get started!</p>
+      <div className="flex flex-col">
+        <button className="transition-colors ease-in-out bg-slate-300 hover:bg-transparent border-2 py-5 px-20 text-xl rounded-2xl mx-10">Sign Up</button>
+        <button className="transition-colors ease-in-out bg-transparent hover:bg-slate-300 border-2 py-5 px-20 m-5 text-xl rounded-2xl mx-10 ">Log In</button>
+      </div>
+      
       
       
       <p className="text-center">2024 Guido Asbun, Adrian Vazquez, & William Bui</p>
--- src/app/sign-in/[[...sign-in]]/page.jsx
+++ src/app/sign-in/[[...sign-in]]/page.jsx
@@ -4,6 +4,7 @@ export default function Page() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <SignIn />;
+      
     </main>
   );
 }
