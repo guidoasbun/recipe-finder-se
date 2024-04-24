1f158b79e938bcb4b3bb1831ac4ab73978006b73
adriancancode <vazqueza2004@gmail.com>
2024-04-23 18:17:58 -0700

    Fix up sign in and sign up page

--- src/app/sign-in/[[...sign-in]]/page.jsx
+++ src/app/sign-in/[[...sign-in]]/page.jsx
@@ -2,9 +2,10 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
   return (
+    
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <SignIn />;
-      
-    </main>
+    <SignIn />;
+  </main>
+   
   );
 }
--- src/app/sign-up/[[...sign-up]]/page.jsx
+++ src/app/sign-up/[[...sign-up]]/page.jsx
@@ -1,11 +1,5 @@
 import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <SignUp />;
-      
-    </main>
-    
-  );
+  return <SignUp />;
 }
