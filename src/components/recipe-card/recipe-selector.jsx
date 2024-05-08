97fa3274db0f35f5ac1961d54ad28eb4cdbf1f76
Guido Asbun <guido@asbun.io>
2024-05-08 16:02:29 -0700

    Fixed recipe selector spacing

--- src/components/recipe-card/recipe-selector.jsx
+++ src/components/recipe-card/recipe-selector.jsx
@@ -10,22 +10,25 @@ export default function RecipeSelector() {
 
   return (
     <div>
-      <main className="flex flex-col items-center justify-between p-4 md:p-24">
-        <p className="text-4xl font-bold text-center pb-10">Select a recipe</p>
-        <div className="flex flex-wrap justify-center">
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-4 md:gap-10 w-full max-w-7xl">
-            <div className="py-5">
+      <div>
+        <main className="flex flex-col items-center justify-center p-4 md:p-24">
+          <p className="text-4xl font-bold text-center pb-10">
+            Select a recipe
+          </p>
+
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 w-full max-w-7xl">
+            <div className="md:col-span-1 lg:col-span-1">
               <CardDefault props={recipe1} />
             </div>
-            <div className="py-5">
+            <div className="md:col-span-1 lg:col-span-1">
               <CardDefault props={recipe2} />
             </div>
-            <div className="py-5">
+            <div className="md:col-span-1 lg:col-span-1">
               <CardDefault props={recipe3} />
             </div>
           </div>
-        </div>
-      </main>
+        </main>
+      </div>
     </div>
   );
 }
